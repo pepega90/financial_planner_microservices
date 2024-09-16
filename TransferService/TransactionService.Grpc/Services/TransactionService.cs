f756a802f49c2a24e839848ef4768a1e5217efd2
pepega90 <asuarif5@gmail.com>
2024-09-17 04:33:07 +0700

    feat: initial create transaction service gRPC

---
+++ TransferService/TransactionService.Grpc/Services/TransactionService.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Core;
+using MassTransit;
+using TransactionService.Protos;
+
+namespace TransactionService.Grpc.Services
+{
+    public class TransactionService : Protos.TransactionService.TransactionServiceBase
+    {
+        public readonly IPublishEndpoint _publishEndpoint;
+        public TransactionService(IPublishEndpoint publishEndpoint)
+        {
+            _publishEndpoint = publishEndpoint;
+        }
+        public override Task<TransferResponse> Transfer(TransferRequest request, ServerCallContext context)
+        {
+            return base.Transfer(request, context);
+        }
+    }
+}
